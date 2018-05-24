2a43cc07ab80ee3838060b6fb53d158c01f187b9
ZeKittyCodes <39416672+ZeKittyCodes@users.noreply.github.com>
2018-05-24 12:20:27 -0700

    Update GuiMethods.java

--- GuiMethods.java
+++ GuiMethods.java
@@ -12,27 +12,32 @@ import javax.swing.event.*;     // access to JSlider events
 public class GuiMethods extends JFrame
 {
     // instance variables
-    private final static int numRow = 5;      //horizontal gap
-    private final static int numCol = 5;      //vertical gap
+    private final static int numRow = 14;      //horizontal gap
+    private final static int numCol = 9;      //vertical gap
+    
     private JTextArea textArea1, textArea2;     //objects of JTextArea type
     private int xPos, yPos;     //position of mouse
 
     /**
-     * Constructs a new instance of GuiMethods
+     * Constructs a new game grid
      */
     public GuiMethods()
     {
         super();    //calls super
         Container contain = getContentPane();   //creates an object for a method
         contain.setLayout(new GridLayout(numRow, numCol));    //sets BorderLayout; gaps between components >= 0
+        
         textArea1 = new JTextArea(2, 10);       //text area size 2 x 10
         textArea1.setText("Click the Mouse");   //tells player to press a button
         textArea1.setEnabled(false);    //makes it so that player can't type in text area
         textArea1.setBackground(Color.BLUE);    //sets background color as blue
+        
         contain.add(textArea1);   //adds border components 
         contain.add(textArea2, GridLayout.SOUTH);   //adds border components
+        
         addKeyListener(new KeyHandler());
         addMouseListener(new MouseClickHandler());
+        
         addWindowListener(new java.awt.event.WindowAdapter()
             {
                 public void windowClosing(WindowEvent evt){
@@ -40,15 +45,19 @@ public class GuiMethods extends JFrame
                 }
             }
         );
+        
         setSize(500, 400);      //width= 500, height= 400
         setVisible(true);
     }
+    /** METHOD EXPLINATION HERE **/
     public void paint(Graphics g)
     {
         super.paint(g);     //calls super method paint
         g.drawString("Clicked at [" + xPos + ", " + yPos + "]", xPos, yPos);
     }
-    public static void main(String[]args){
+    /** METHOD EXPLINATION HERE **/
+    public static void main(String[]args)
+    {
         GuiMethods application = new GuiMethods();
     }
 }
