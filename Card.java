4034c0782067499d97f402daf7252e313bfeaf04
JohnYu11 <39281962+JohnYu11@users.noreply.github.com>
2018-05-27 16:27:04 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,7 +1,6 @@
-
 /**
  * FINAL PROJECT
- * John YU, Varshini Sathish
+ * John Yu, Varshini Sathish
  * 4/26/2018
  * 
  * Description: This class represents the attributes 
@@ -11,68 +10,65 @@
  * can be used to compare to other cards.
  */
 public class Card
-{
+{      
     private int myNumber; //initialize the attributes of a card
-    private String mySuit;
-    private boolean faceUp; //true if faceUp, false otherwise	
+    private String mySuit;  //either d,c,s, or h
+    private boolean faceUp; //true if faceUp, false otherwise   
 
     public Card(int number, String suit)
     {
         myNumber = number; //set variables to instance of card
         mySuit = suit;
-	faceUp = false;    
+        faceUp = false;    
     }
 
     /**GET VARIABLES**/
-    
+
     public int getNumber()
     {
         return myNumber;
     }
 
-    public String getType()
-    {
-        return myType;
-    }
- // card face methods
+    // card face methods
     public void turnFaceDown()
     {
-	faceUp = false; 
+        faceUp = false; 
     }
-	
+
     public void turnFaceUp()
     {
-	faceUp = true; 
+        faceUp = true; 
     }
+
     public boolean isFaceUp()
     {
-	return  isFaceUp;  
-    }	    
-	
+        return  faceUp;  
+    }       
+
     //if black color
     public boolean isBlack()
     {
-    	return (mySuit.equals("s") || mySuit.equals("c")); 	    
-    }	 
-	
+        return (mySuit.equals("s") || mySuit.equals("c"));      
+    }    
+
     /**
      *Finds appropriate file name for gif corresponding to card based
-     * on suit and rank
+     * on suit and myNumber
      *
      * @return name of the file for the appropriate Card
      */
     public String getFileName()
     {
-        if (!isFaceUp)
-	{
-		return "backcard.gif"; //using escape sequence, not double slash
-	}
-        if (rank == 10) return "t" + suit + ".gif";
-        if (rank == 11) return "j" + suit + ".gif";
-        if (rank == 12) return "q" + suit + ".gif";
-        if (rank == 13) return "k" + suit + ".gif";
-        if (rank == 1) return "a" + suit + ".gif";
-        return rank + suit + ".gif";
+        if (!faceUp)
+        {
+            return "backcard.gif"; //using escape sequence, not double slash
+        }
+        if (myNumber == 10) return "t" + mySuit + ".gif";
+        if (myNumber == 11) return "j" + mySuit + ".gif";
+        if (myNumber == 12) return "q" + mySuit + ".gif";
+        if (myNumber == 13) return "k" + mySuit + ".gif";
+        if (myNumber == 1) return "a" + mySuit + ".gif";
+        return myNumber + mySuit + ".gif";
     }    
-	    
+
 }
