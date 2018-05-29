712f7cbd747f7668ae13a3e8a01bbd06b819bcb7
JohnYu11 <39281962+JohnYu11@users.noreply.github.com>
2018-05-29 12:03:55 -0700

    Update GuiMethods.java

--- GuiMethods.java
+++ GuiMethods.java
@@ -12,14 +12,14 @@ import java.io.*;           //access to input/output
 import javax.swing.*;       // access to JFrame and Jcomponents
 import java.util.*;
 
-public class GuiMethods extends JComponent implements MouseListener
+public class GuiMethodsMain extends JComponent implements MouseListener
 {
     // instance variables
-    private static final int cardwide = 73;
-    private static final int cardhigh = 97;
-    private static final int space = 5;  //distance between cards
-    private static final int up_offset = 15;  //distance for cascading face-up cards
-    private static final int down_offset = 5;  //distance for cascading face-down cards
+    private static final int cardwide = 70;
+    private static final int cardhigh = 95;
+    private static final int space = 10;  //distance between cards
+    private static final int up_offset = 20;  //distance for cascading face-up cards
+    private static final int down_offset = 10;  //distance for cascading face-down cards
 
     private JFrame frame;   //creates object for JFrame
     private int selectedRow = -1;
@@ -29,7 +29,7 @@ public class GuiMethods extends JComponent implements MouseListener
     /**
      * Constructs a new instance of GuiMethods
      */
-    public GuiMethods(Game game)
+    public GuiMethodsMain(Game game)
     {
         this.game = game;
 
@@ -37,8 +37,8 @@ public class GuiMethods extends JComponent implements MouseListener
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(this);
 
-        this.setPreferredSize(new Dimension(cardwide * 7 + 
-                space * 8, cardhigh * 2 + space * 3 
+        this.setPreferredSize(new Dimension(cardwide * 12 + 
+                space * 12, cardhigh * 2 + space * 3 
                 + down_offset * 7 + 13 * up_offset));
         this.addMouseListener(this);
 
@@ -48,30 +48,30 @@ public class GuiMethods extends JComponent implements MouseListener
 
     public void paint(Graphics g)
     {
-        g.setColor(new Color(0, 128, 0));
-        g.fillRect(0, 0, getWidth(), getHeight());
-        //face down
+        g.setColor(new Color(81, 2, 39));
+        g.fillRect(cardwide+space, 0, getWidth()- 2*(space + cardwide), getHeight());
+        //hand
         drawCard(g, game.getHand(), space, space);
 
-        //stock
-        drawCard(g, game.getTalon(), space * 2 + cardwide, space);
+        //talon
+        drawCard(g, game.getTalon(), space, space * 7 + cardhigh);
         if (selectedRow == 0 && selectedCol == 1)
             drawBorder(g, space * 2 + cardwide, space);
 
-        //aces
-        for (int i = 0; i < 4; i++)
-            drawCard(g, game.getFoundation(i), space * (4 + i) + cardwide * (3 + i), space);
+        //foundation
+        for (int k = 0; k < 4; k++)
+            drawCard(g, game.getFoundation(k), getWidth()-(cardwide+2*space),(cardhigh+2*space));
 
-        //piles
-        for (int i = 0; i < 7; i++)
+        //tableau
+        for (int n = 0; n < 7; n++)
         {
-            Stack<Card> pile = game.getTableau(i);
+            Stack<Card> pile = game.getTableau(n);
             int offset = 0;
             for (int j = 0; j < pile.size(); j++)
             {
-                drawCard(g, pile.get(j), space + (cardwide + space) * i, cardhigh + 2 * space + offset);
-                if (selectedRow == 1 && selectedCol == i && j == pile.size() - 1)
-                    drawBorder(g, space + (cardwide + space) * i, cardhigh + 2 * space + offset);
+                drawCard(g, pile.get(j), 11* space + ((cardwide) + space * 4) * n, cardhigh + 2 * space + offset);
+                if (selectedRow == 1 && selectedCol == n && j == pile.size() - 1)
+                    drawBorder(g, 11* space + ((cardwide) + space * 4) * n, cardhigh + 2 * space + offset);
 
                 if (pile.get(j).isFaceUp())
                     offset += up_offset;
@@ -83,7 +83,7 @@ public class GuiMethods extends JComponent implements MouseListener
 
     private void drawBorder(Graphics g, int x, int y)
     {
-        g.setColor(Color.YELLOW);
+        g.setColor(Color.GRAY);
         g.drawRect(x, y, cardwide, cardhigh);
         g.drawRect(x + 1, y + 1, cardwide - 2, cardhigh - 2);
         g.drawRect(x + 2, y + 2, cardwide - 4, cardhigh - 4);
@@ -162,7 +162,7 @@ public class GuiMethods extends JComponent implements MouseListener
 
     public boolean isTableauSelected()
     {
-        return selectedRow == 1;
+        return selectedRow == 0;
     }
 
     public int selectedTableau()
